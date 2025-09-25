73decf1982d9aa5ae72ae7077ffa100b445d2b9b
zzang-sera <rijooon@gmail.com>
2025-09-25 21:42:48 +0800

    폰트 최종수정

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
   },
   textBase: {
     fontFamily: 'NotoSansKR',
-    fontWeight: '500',
+    fontWeight: '700',
   },
   // --- Type: 'feature' (가까운 역 안내 등) ---
   featureButton: {
